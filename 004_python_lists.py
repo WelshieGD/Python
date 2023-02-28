32938af92a5f3e45ffa70cf8c514dcb45e551930
Graham Davies <graham@grahamdavies.info>
2023-02-28 09:01:49 +0000

    Lists

--- 004_python_lists.py
+++ 004_python_lists.py
@@ -14,17 +14,23 @@ print(f"The first bass guitar in UPPER CASE is {bassguitars[0].upper()}\n")
 
 # endregion changingcase
 
+# region selectinganitembasedonindex
 # Last item - Fender
 print(f"The last bass guitar manufacturer in the list is {bassguitars[-1]}\n")
 
 # Third last item - Fender
 print(f"The third from last bass guitar manufacturer in the list is {bassguitars[-3]}\n")
 
+# endregion selectinganitembasedonindex
+
+# region updatinganitembasedonindex
 # Rename (update) an item in a list (Fender --> Squire) 
 bassguitars[1] = "Squire"
 print(f"Swapped out Fender for {bassguitars[1]}")
 print(f"List of bass guitar manufacturers is now {bassguitars}\n")
+# endregion updatinganitembasedonindex
 
+# region addelementstolist
 # Add elements to the end of a list
 bassguitars.append('Fender')
 print(f"Added {bassguitars[-1]}")
@@ -34,6 +40,9 @@ print(f"The list is now {bassguitars}\n")
 bassguitars.insert(3,'Cort')
 print(f"The list is now {bassguitars}\n")
 
+# endregion addelementstolist
+
+# region removeelementsfromlist
 # Remove an item from a list based on index
 print(f"The bass guitar manufacturer we are about to delete is {bassguitars[1]}")
 del bassguitars[1]
@@ -45,6 +54,11 @@ print(f"The bass guitar manufacturer we are about to delete is Ibanez")
 bassguitars.remove('Ibanez')
 print(f"The current list of bass guitar manufacturers is now {bassguitars}\n")
 
+# endregion removeelementsfromlist
+
+# region popitems
+
+
 
 # Remove last item on list but still be able to reference it using pop[]
 popped_bassguitars = bassguitars.pop()
@@ -55,3 +69,5 @@ print (f"The bass guitar manufacture last deleted (popped off the list) is {popp
 popped_bassguitars = bassguitars.pop(1)
 print(f"The current list of bass guitar manufacturers is {bassguitars}")
 print (f"The bass guitar manufacture last deleted (popped off the list) is {popped_bassguitars}\n")
+
+# endregion popitems
