7c353a36099cc26cf60ae321fba8fe7912241feb
Graham Davies <graham@grahamdavies.info>
2023-02-28 10:07:44 +0000

    Lists

--- 004_python_lists.py
+++ 004_python_lists.py
@@ -21,6 +21,9 @@ print(f"The last bass guitar manufacturer in the list is {bassguitars[-1]}\n")
 # Third last item - Fender
 print(f"The third from last bass guitar manufacturer in the list is {bassguitars[-3]}\n")
 
+# Note if there are fewer items in list than what you are asking for then index error. 
+# E.g. [-1] of an empty list or [-3] of a list with 2 items will generate an error.
+
 # endregion selectinganitembasedonindex
 
 # region updatinganitembasedonindex
@@ -31,7 +34,7 @@ print(f"List of bass guitar manufacturers is now {bassguitars}\n")
 # endregion updatinganitembasedonindex
 
 # region addelementstolist
-# Add elements to the end of a list
+# Add elements to the end of a list - ** can only append one item at a time **
 bassguitars.append('Fender')
 print(f"Added {bassguitars[-1]}")
 print(f"The list is now {bassguitars}\n")
@@ -58,8 +61,6 @@ print(f"The current list of bass guitar manufacturers is now {bassguitars}\n")
 
 # region popitems
 
-
-
 # Remove last item on list but still be able to reference it using pop[]
 popped_bassguitars = bassguitars.pop()
 print(f"The current list of bass guitar manufacturers is {bassguitars}")
@@ -71,3 +72,35 @@ print(f"The current list of bass guitar manufacturers is {bassguitars}")
 print (f"The bass guitar manufacture last deleted (popped off the list) is {popped_bassguitars}\n")
 
 # endregion popitems
+
+
+#region sortinglists
+# Be aware that case sensitivity might make this more complex than simple examples below
+#Permanently with sort() method
+
+print(f"\nThe current list of bass guitar manufacturers is {bassguitars}")
+bassguitars.sort()
+print(f"\nThe list of bass guitar manufacturers sorted alphabetically is {bassguitars}")
+bassguitars.sort(reverse=True)
+print(f"\nThe list of bass guitar manufacturers sorted reverse alphabetically is {bassguitars}")
+bassguitars.sort()
+# Temporarily with sorted() method
+print(f"\nThe list of bass guitars in alphabetical order is {sorted(bassguitars)}")
+
+# reverse order of list (not alphabetically - just the order)
+bassguitars.append ('Fender')
+bassguitars.append ('Squire')
+bassguitars.append ('Sire')
+print(f"\nThe list is now {bassguitars}")
+
+# .reverse() uses in place modification. You cannot print it directly. You need to modify the list first. 
+bassguitars.reverse()
+print(f"\nThe list in reverse is {bassguitars}")
+
+# Number of items in a list
+numbasses = len(bassguitars)
+print(f"\nI now have {numbasses} bass guitars")
+
+
+#endregion sortinglists
+
